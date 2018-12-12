845c169e8823ef7b7a6023e0d36432b5da1e6bff
buttslapper <jfranny92@aol.com>
2018-12-11 17:27:58 -0800

    Changed the default page to the middle fragment. Truck yeah.

--- app/src/main/java/com/example/johnkaehler/ski_buds/MainActivity.java
+++ app/src/main/java/com/example/johnkaehler/ski_buds/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -21,6 +22,9 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentPagerAdapter adapterViewPager;
+    ViewPager viewPager;
+
     private static final int RC_SIGN_IN = 123;
     List providers = Arrays.asList(
             new AuthUI.IdpConfig.EmailBuilder().build());
@@ -30,7 +34,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivityForResult(
+        viewPager = findViewById(R.id.vpPager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapterViewPager);
+        viewPager.setCurrentItem(1);
+
+            startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
@@ -56,9 +65,15 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                MiddleFragment middleFragment = new MiddleFragment();
-                ft.add(R.id.fragment_container, middleFragment).commit();
+                String displayName = user.getDisplayName();
+
+
+                //Bundle bundle = new Bundle();
+                //bundle.putString("display_name", displayName);
+                //FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                //MiddleFragment middleFragment = new MiddleFragment();
+                //middleFragment.setArguments(bundle);
+                //ft.add(R.id.fragment_container, middleFragment).commit();
                 // ...
             } else {
                 // Sign in failed. If response is null the user canceled the
@@ -69,5 +84,30 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public static class MyPagerAdapter extends FragmentPagerAdapter{
+        private final static int NUM_ITEMS = 3;
 
+        public MyPagerAdapter(FragmentManager fragmentManager){
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return LeftFragment.newInstance();
+                case 1:
+                    return MiddleFragment.newInstance();
+                case 2:
+                    return RightFragment.newInstance();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+    }
 }
